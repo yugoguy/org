1bf24d2687d8ec933c37b7276236af808c5bea64
yugoguy <133622083+yugoguy@users.noreply.github.com>
2026-01-25 00:17:54 +0000

    Add initialization options for LVE class

--- dev/MainLVE.py
+++ dev/MainLVE.py
@@ -1,6 +1,5 @@
-#@title LVE
-
 import numpy as np
+import random
 import torch
 
 class LVE:
@@ -12,13 +11,18 @@ class LVE:
         latent_module: LatentModule instance
         toolbox: DEAP toolbox for latent space GA
         device: 'cpu' or 'cuda'
+        init_from_dataset: if True, initialize latent pop from encoded dataset
+        init_epsilon: noise scale added to mu (z = mu + epsilon * std)
     """
     
-    def __init__(self, data_generation, latent_module, toolbox, device='cpu'):
+    def __init__(self, data_generation, latent_module, toolbox, device='cpu',
+                 init_from_dataset=False, init_epsilon=0.0):
         self.data_generation = data_generation
         self.latent_module = latent_module
         self.toolbox = toolbox
         self.device = device
+        self.init_from_dataset = init_from_dataset
+        self.init_epsilon = init_epsilon
         self.dataset = None
     
     def regenerate(self):
@@ -81,6 +85,45 @@ class LVE:
             x = self.latent_module.decode(z)
         return x.cpu().numpy()
     
+    def _sample_latent_from_dataset(self):
+        """Sample one latent vector from encoded dataset."""
+        idx = random.randint(0, len(self.dataset) - 1)
+        x = torch.tensor(self.dataset[idx], dtype=torch.float32).unsqueeze(0).to(self.device)
+        self.latent_module.eval()
+        with torch.no_grad():
+            if hasattr(self.latent_module, 'encode_dist'):
+                mu, logvar = self.latent_module.encode_dist(x)
+                std = torch.exp(0.5 * logvar)
+                z = mu + self.init_epsilon * std
+            else:
+                z = self.latent_module.encode(x)
+        return z.squeeze(0).cpu().tolist()
+    
+    def init_population(self, pop_size):
+        """
+        Initialize latent population.
+        
+        Args:
+            pop_size: population size
+        
+        Returns:
+            list of DEAP individuals in latent space
+        """
+        from deap import tools
+        
+        if self.init_from_dataset and self.dataset is not None:
+            pop = []
+            for _ in range(pop_size):
+                z = self._sample_latent_from_dataset()
+                ind = self.toolbox.clone(self.toolbox.individual())
+                for i in range(len(z)):
+                    ind[i] = z[i]
+                del ind.fitness.values
+                pop.append(ind)
+            return pop
+        else:
+            return self.toolbox.population(n=pop_size)
+    
     def evolve(self, pop_size, n_gen, evolve_fn):
         """
         Main LVE loop.
@@ -89,7 +132,7 @@ class LVE:
             pop_size: population size
             n_gen: number of generations
             evolve_fn: function(toolbox, lve, pop_size, n_gen) -> final population
-                       lve is passed so evolve_fn can use encode/decode
+                       lve is passed so evolve_fn can use encode/decode/init_population
         
         Returns:
             final population in original space (numpy array)
