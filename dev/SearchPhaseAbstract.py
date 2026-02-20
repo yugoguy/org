03c59a02cb8fe45617fe3ec2de1d6999e604011e
yugoguy <133622083+yugoguy@users.noreply.github.com>
2026-02-20 20:26:00 +0000

    Create SearchPhaseAbstract.py

---
+++ dev/SearchPhaseAbstract.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+
+class SearchPhase(ABC):
+    """
+    Abstract class for search phase in SSLVE.
+
+    At t=0: sample(behavior_matching=None) generates initial dataset Theta_0.
+    At t>0: sample(behavior_matching) uses current bins and dataset to generate new samples.
+
+    Args:
+        problem: ProblemSetting instance
+    """
+
+    def __init__(self, problem):
+        self.problem = problem
+
+    @abstractmethod
+    def sample(self, behavior_matching=None):
+        """
+        Generate new samples.
+
+        Args:
+            behavior_matching: BehaviorMatching instance (None at t=0)
+
+        Returns:
+            list of numpy arrays (new candidate genes)
+        """
+        pass
