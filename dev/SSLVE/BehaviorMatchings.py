71c023ddebf95346ccd2b8d260d0bd7967f7921d
yugoguy <133622083+yugoguy@users.noreply.github.com>
2026-02-21 17:49:44 +0000

    Update BehaviorMatchings.py

--- dev/SSLVE/BehaviorMatchings.py
+++ dev/SSLVE/BehaviorMatchings.py
@@ -1,5 +1,46 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
+
+
+class BehaviorMatching(ABC):
+    """
+    Abstract class for behavior-matching archive management.
+
+    Maintains:
+        dataset   : list of numpy arrays (stored thetas)
+        fitnesses : list of floats
+        bin_ids   : list of bin IDs, bin_ids[i] is bin of dataset[i]
+        bins      : dict {bin_id: [(theta, fitness), ...]}
+        bins_idx  : dict {bin_id: [dataset_indices]}
+
+    Args:
+        behavior_descriptor: BehaviorDescriptor instance
+        fitness_fn: callable(info_dict) -> float (scalar fitness, to minimize)
+    """
+
+    def __init__(self, behavior_descriptor, fitness_fn):
+        self.behavior_descriptor = behavior_descriptor
+        self.fitness_fn = fitness_fn
+        self.dataset = []
+        self.fitnesses = []
+        self.bin_ids = []
+        self.bins = {}
+        self.bins_idx = {}
+
+    @abstractmethod
+    def update(self, thetas, infos):
+        """
+        Update archive with new candidates.
+
+        Args:
+            thetas: list of numpy arrays
+            infos: list of info dicts from Collector.collect()
+
+        Updates self.dataset, self.fitnesses, self.bin_ids, self.bins, self.bins_idx in place.
+        """
+        pass
+        
 
 class MAPElitesBM:
     """
