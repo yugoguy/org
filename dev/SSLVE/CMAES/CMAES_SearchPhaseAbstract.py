e6d497db2ee6dc195fa14c93c0a5e1d8cc017abe
yugoguy <133622083+yugoguy@users.noreply.github.com>
2026-02-21 14:56:57 +0000

    Update class description for CMAES_SearchPhase

--- dev/SSLVE/CMAES/CMAES_SearchPhaseAbstract.py
+++ dev/SSLVE/CMAES/CMAES_SearchPhaseAbstract.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 
 class CMAES_SearchPhase(ABC):
     """
-    Abstract class for search phase in SSLVE.
+    Abstract class for search phase in CMAES.
 
     At t=0: sample(behavior_matching=None) generates initial dataset Theta_0.
     At t>0: sample(behavior_matching) uses current bins and dataset to generate new samples.
